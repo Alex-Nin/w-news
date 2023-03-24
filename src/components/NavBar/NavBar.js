38720b6b36dc1bb28446d3e8205443506453ad88
Alex-Nin <nin.alex.n@gmail.com>
2023-03-23 20:40:37 -0600

    updating navigation

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -24,8 +24,8 @@ const NavBar = () => {
       <svg onClick={toggleMenu} id='close-menu' width="32" height="31" xmlns="http://www.w3.org/2000/svg"><g fill="#00001A" fill-rule="evenodd"><path d="m2.919.297 28.284 28.284-2.122 2.122L.797 2.419z"/><path d="M.797 28.581 29.081.297l2.122 2.122L2.919 30.703z"/></g></svg>
         <ul>
           <li onClick={toggleMenu}><Link to="/w-news">Home</Link></li>
-          <li onClick={toggleMenu}><Link to="new">New</Link></li>
-          <li onClick={toggleMenu}><Link to="trending">Trending</Link></li>
+          <li onClick={toggleMenu}><Link to="/w-new/new">New</Link></li>
+          <li onClick={toggleMenu}><Link to="/w-new/trending">Trending</Link></li>
         </ul>
       </nav>
       <svg onClick={toggleMenu} id='open-menu' width="40" height="17" xmlns="http://www.w3.org/2000/svg"><g fill="#00001A" fill-rule="evenodd"><path d="M0 0h40v3H0zM0 7h40v3H0zM0 14h40v3H0z"/><path d="M0 0h40v3H0z"/></g></svg>
